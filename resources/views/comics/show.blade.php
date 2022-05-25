736f6a8ae11f60dbb9e3942d44bf7c16450f0a82
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-25 17:14:11 +0200

    update show.blade.php

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,29 @@
 @extends('layout.app')
-@section('title', 'Home')
+@section('title', 'comic')
 
 @section('detail')
 
-    @dump($comic->description)
+    {{-- @dump($comic) --}}
+
+
+    <div class="container">
+
+
+        <div class="card mb-3 mt-5" style="max-width: 1350px" ;>
+            <div class="row g-0 d-flex flex-direction-center">
+                <div class="col-2">
+                    <img src="{{ $comic->thumb }}" class="img-fluid rounded-start" alt="{{ $comic->thumb }}">
+                </div>
+
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title text-center m-3">{{ $comic->title }}</h5>
+                        <p class="card-text ">{{ $comic->description }}</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
