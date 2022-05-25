7474695532945064c24c8aca1bce25f8aa75f77c
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-25 15:18:10 +0200

    update comics.index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,5 +4,31 @@
 
 @section('content')
 
-@dump($comics)
+    @dump($comics)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data di Uscita</th>
+                <th scope="col">Tipo</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic->title }}</th>
+                    <td>{{ $comic->price }} $ </td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                    <td>{{ $comic->type }}</td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+
 @endsection
